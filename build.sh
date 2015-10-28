69747f38806c8eb4bb4e10f75275d0d30c65a918
Kevin Schoon <kevinschoon@gmail.com>
2015-10-28 19:49:57 -0400

    Update build.sh packaging

--- build.sh
+++ build.sh
@@ -1,39 +1,36 @@
 #!/usr/bin/env bash
+set -e
 # Create a Python package to deploy to AWS Lambda.
 
 PACKAGE=$1
 BUILD_DIR="./_build"
+PACKAGE_SHA=$(git rev-parse HEAD)
+PACKAGE_DIR="./package"
+PACKAGE_PATH="$PACKAGE_DIR/$PACKAGE_SHA-$PACKAGE.zip"
 
 if [ ! "$PACKAGE" ]; then
     echo "You need to specify a package name"
     exit 1
 fi
 
-echo "Building $PACKAGE"
+if [ ! -d "$PACKAGE_DIR" ]; then
+    mkdir -v "$PACKAGE_DIR"
+fi
 
 if [ -d "$BUILD_DIR" ]; then
     rm -Rv "$BUILD_DIR"
+    mkdir -v "$BUILD_DIR"
+fi
+
+if [ -d "$PACKAGE" ]; then
+    pushd "$PACKAGE"
+    zip -r "../$PACKAGE_PATH" *
+    popd
 fi
 
 if [ -f "$PACKAGE/requirements.txt" ]; then
-    mkdir -v "$BUILD_DIR"
     pip install -r "$PACKAGE/requirements.txt" -t "$BUILD_DIR"
-    pushd "$PACKAGE"
-    zip -r "$PACKAGE.zip" *
-    mv -v "$PACKAGE.zip" ..
-    popd
-fi
-
-if [ -d "$BUILD_DIR" ]; then
     pushd "$BUILD_DIR"
-    zip -r --grow "../$PACKAGE.zip" *
-    popd
+    zip -r --grow "../$PACKAGE_PATH" *
 fi
 
-if [ -f "$PACKAGE/package.json" ]; then
-    pushd "$PACKAGE"
-    npm install
-    zip -r "$PACKAGE" *
-    mv -v "$PACKAGE.zip" ..
-    popd
-fi
