efd3cb6e83b9709a1d56534bb75af8178660e0c9
Bradley Cicenas <bradley.cicenas@gmail.com>
2015-10-29 15:25:57 -0400

    update build script

--- build.sh
+++ build.sh
@@ -1,36 +1,64 @@
-#!/usr/bin/env bash
-set -e
+#!/bin/bash
 # Create a Python package to deploy to AWS Lambda.
 
-PACKAGE=$1
-BUILD_DIR="./_build"
-PACKAGE_SHA=$(git rev-parse HEAD)
-PACKAGE_DIR="./package"
-PACKAGE_PATH="$PACKAGE_DIR/$PACKAGE_SHA-$PACKAGE.zip"
+installed () {
+  command -v "$1" >/dev/null 2>&1
+}
 
-if [ ! "$PACKAGE" ]; then
-    echo "You need to specify a package name"
+githead () {
+  cd $1
+  git rev-parse HEAD >/dev/null 2>&1
+}
+
+package() {
+  echo "Creating package..."
+  cd $2 && zip -qr $1 * && cd -
+}
+
+package-add() {
+  echo "Adding build files to package..."
+  cd $2 && zip -qr --grow $1 * && cd -
+}
+
+[ $# != 1 ] && {
+  echo "usage: $0 <path-to-package>"
+  exit 1
+}
+
+TARGET_DIR=$(realpath $1)
+CURRENT_DIR=$PWD
+
+for cmd in pip2 zip git; do
+  ! installed $cmd && {
+    echo "error: $cmd not found"
     exit 1
+  }
+done
+
+if [ -z "$(githead $TARGET_DIR)" ]; then
+  PACKAGE_NAME="${TARGET_DIR}-latest.zip"
+else
+  PACKAGE_NAME="${TARGET_DIR}-$(githead $TARGET_DIR).zip"
 fi
 
-if [ ! -d "$PACKAGE_DIR" ]; then
-    mkdir -v "$PACKAGE_DIR"
+if [ -f ${PACKAGE_NAME} ]; then
+  echo "$(basename ${PACKAGE_NAME}) exists in the current directory"
+  read -p "overwrite?(y/n)" confirm
+  [ "$confirm" != "y" ] && {
+    echo "aborted"
+    exit 1
+  }
+  rm -f ${PACKAGE_NAME}
 fi
 
-if [ -d "$BUILD_DIR" ]; then
-    rm -Rv "$BUILD_DIR"
-    mkdir -v "$BUILD_DIR"
+package $PACKAGE_NAME $TARGET_DIR
+
+if [ -f "${TARGET_DIR}/requirements.txt" ]; then 
+  echo "Packaging pip requirements..."
+  TMPDIR=$(mktemp -d /tmp/lambdapack.XXXX)
+  pip2 install -r ${TARGET_DIR}/requirements.txt -t $TMPDIR 1>> ${TMPDIR}/.buildlog
+  package-add $PACKAGE_NAME $TMPDIR
 fi
 
-if [ -d "$PACKAGE" ]; then
-    pushd "$PACKAGE"
-    zip -r "../$PACKAGE_PATH" *
-    popd
-fi
-
-if [ -f "$PACKAGE/requirements.txt" ]; then
-    pip install -r "$PACKAGE/requirements.txt" -t "$BUILD_DIR"
-    pushd "$BUILD_DIR"
-    zip -r --grow "../$PACKAGE_PATH" *
-fi
-
+echo "done"
+exit 0
